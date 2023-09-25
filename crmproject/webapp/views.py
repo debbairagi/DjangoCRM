ab86565f2422db2e464b3328c30afc324e95b529
debbairagi <freelancer.debbindu@gmail.com>
2023-09-25 11:09:44 +0600

    changes the in webapp

--- crmproject/webapp/views.py
+++ crmproject/webapp/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from webapp.forms import CreateUserForm, LoginForm
 
 # Create your views here.
 
 def home(request):
     # return HttpResponse("HEllow World")
     return render(request, 'webapp/index.html', {})
+
+def register_view(request):
+    register_form = CreateUserForm()
+    diction = {'register_form': register_form}
+    return render(request, 'webapp/register-page.html', context=diction)
